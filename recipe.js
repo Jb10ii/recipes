8aa70ec5bf1568c7d611480e6ce521eae9422f0e
Jibril <jibrilballer34@gmail.com>
2024-09-16 19:22:06 +0300

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,9 +1,9 @@
 const food = [] 
 
 for (let i = 0; i < 3; i++) {
-    students.push(prompt(`Enter the name of your recipe ${i+1}`))
-     students.push(prompt(`Enter the name of your ingredients ${i+1}`))
-     students.push(prompt(`Enter the name of your instructions ${i+1}`))
+    food.push(prompt(`Enter the name of your recipe ${i+1}`))
+    food.push(prompt(`Enter the name of your ingredients ${i+1}`))
+    food.push(prompt(`Enter the name of your instructions ${i+1}`))
     
 }
 
