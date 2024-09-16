f4c78821aae406d07f49ced5fa8fad9535f1ea72
J M <jm@Js-MacBook-Air.local>
2024-09-16 19:14:54 +0300

    new recipe code

---
+++ recipe.js
@@ -0,0 +1,10 @@
+const students = [] 
+
+for (let i = 0; i < 3; i++) {
+    students.push(prompt(`Enter the name of your recipe ${i+1}`))
+     students.push(prompt(`Enter the name of your ingredients ${i+1}`))
+     students.push(prompt(`Enter the name of your instructions ${i+1}`))
+    
+}
+
+console.log(students);
